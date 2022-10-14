c4bfcfc91c51896df162e774c0bfa400b351123c
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-10-14 16:49:33 -0400

    v0.0.13 - time parser

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import { uploadAttachment } from "./attachments";
 import { AutocompleteItems, AutocompleteType, parseAutocomplete } from "./autocomplete";
+import { msToString, stringToMS } from "./timeParser";
 
-export { uploadAttachment, AutocompleteType, AutocompleteItems, parseAutocomplete };
+export {
+  uploadAttachment,
+  AutocompleteType,
+  AutocompleteItems,
+  parseAutocomplete,
+  stringToMS,
+  msToString,
+};
