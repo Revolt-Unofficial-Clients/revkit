ee81809fe5faf5e43471baef90061648f621ed0b
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-12-31 17:22:08 -0500

    use Long for flags

--- src/client/utils/Flags.ts
+++ src/client/utils/Flags.ts
@@ -1,8 +1,16 @@
+import Long from "long";
+
+// Shamelessly copied from revolt.js
+export function bitwiseAndEq(a: number, ...b: number[]) {
+  const value = b.reduce((prev, cur) => prev.or(cur), Long.fromNumber(0));
+  return value.and(a).eq(value);
+}
+
 export class BaseFlags {
   constructor(public map: { [key: number]: string }, public bits: number) {}
 
   public has(flag: number) {
-    return !!(flag & this.bits);
+    return bitwiseAndEq(this.bits, flag);
   }
   public all() {
     return Object.values(this.map)
