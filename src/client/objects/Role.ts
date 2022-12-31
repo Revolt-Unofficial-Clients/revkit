8530b89e63294a53fa6c9d86b73f375ac28ed593
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-12-31 17:16:55 -0500

    role props

--- src/client/objects/Role.ts
+++ src/client/objects/Role.ts
@@ -7,4 +7,23 @@ export default class Role extends BaseObject<APIRole> {
   constructor(client: Client, public server: Server, data: APIRole) {
     super(client, data);
   }
+
+  public get name() {
+    return this.source.name;
+  }
+  public get color() {
+    return this.source.colour ?? null;
+  }
+  public get rank() {
+    return this.source.rank || 0;
+  }
+  public get hoist() {
+    return !!this.source.hoist;
+  }
+  public get permissions() {
+    return {
+      allow: this.source.permissions.a,
+      deny: this.source.permissions.d,
+    };
+  }
 }
