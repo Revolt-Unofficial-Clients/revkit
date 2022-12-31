52721079daa8a745a6964a18e5b4720d68a88136
Meow <50887230+itzTheMeow@users.noreply.github.com>
2022-12-31 11:41:19 -0500

    finish emoji

--- src/client/objects/Emoji.ts
+++ src/client/objects/Emoji.ts
@@ -3,7 +3,21 @@ import Client from "../Client";
 import BaseObject from "./BaseObject";
 
 export class Emoji extends BaseObject<APIEmoji> {
+  public name: string;
+  public animated: boolean;
+  public nsfw: boolean;
+  public creatorID: string;
+
   constructor(client: Client, data: APIEmoji) {
     super(client, data);
+    this.name = data.name;
+    this.animated = !!data.animated;
+    this.nsfw = !!data.nsfw;
+    this.creatorID = data.creator_id;
+    //TODO: parent
+  }
+
+  public get creator() {
+    return this.client; //TODO:
   }
 }
