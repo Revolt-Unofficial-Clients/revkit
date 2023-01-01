ca989813cc03b5ab4ac3c2aa1dadf31b582e8dd9
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-01 13:06:38 -0500

    fetch multiple

--- src/managers/MessageManager.ts
+++ src/managers/MessageManager.ts
@@ -1,4 +1,5 @@
-import { APIMessage } from "../api";
+import { APIRoutes } from "revolt-api/dist/routes";
+import { APIMember, APIMessage, APIUser } from "../api";
 import Client from "../Client";
 import Channel from "../objects/Channel";
 import Message from "../objects/Message";
@@ -23,5 +24,25 @@ export default class MessageManager extends BaseManager<Message> {
     return this.construct(
       await this.client.api.get(`/channels/${<"">this.channel.id}/messages/${<"">id}`)
     );
+  }
+  public async fetchMultiple(
+    params?: (APIRoutes & {
+      method: "get";
+      path: "/channels/{target}/messages";
+    })["params"]
+  ) {
+    const messages = await this.client.api.get(`/channels/${<"">this.channel.id}/messages`, {
+      ...params,
+    });
+    if (params?.include_users) {
+      const items = <{ messages: APIMessage[]; users: APIUser[]; members?: APIMember[] }>messages;
+      items.users.forEach((u) => this.client.users.construct(u));
+      const channel = this.channel;
+      if (channel.isServerBased() && items.members)
+        items.members.forEach((m) => channel.server.members.construct(m));
+      return items.messages.map((m) => this.construct(m));
+    } else {
+      return (<APIMessage[]>messages).map((m) => this.construct(m));
+    }
   }
 }
