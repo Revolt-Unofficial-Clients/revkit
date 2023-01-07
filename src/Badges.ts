1ff0d6a565b221ecd8f9d0abf287e19d0cd493ac
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-07 16:02:58 -0500

    add default badges

--- src/Badges.ts
+++ src/Badges.ts
@@ -16,13 +16,6 @@ export const CustomBadges: CustomBadge[] = [
     image: "https://autumn.revolt.chat/icons/7JNqQdO-7WfHyCrw69ct2nNwoM2QSH-RN4qOWWlrtP?max-side=256",
   },
   {
-    //     isnerd                        FatalErrorCoded
-    ids: ["01EX2NCWQ0CHS3QJF0FEQS1GR4", "01EXAME8DPWQH72RCBPX490ZMQ"],
-    name: "Revolt Founder",
-    description: "Founder of Revolt",
-    image: "https://autumn.revolt.chat/icons/gtc0gJE2S3RvuDhrl2-JeakvgbqEGr2acvBnRTTh6k?max_side=256",
-  },
-  {
     //     infi
     ids: ["01F1WKM5TK2V6KCZWR6DGBJDTZ"],
     name: "this is truly the",
--- src/utils/UserBadges.ts
+++ src/utils/UserBadges.ts
@@ -20,6 +20,7 @@ export interface CustomBadge {
   description: string;
   image: string;
 }
+export type UserBadge = Omit<CustomBadge, "ids">;
 
 export class UserBadges extends BaseFlags {
   constructor(bits: number) {
@@ -32,3 +33,62 @@ export class UserBadges extends BaseFlags {
     return super.all();
   }
 }
+
+export const RevoltBadgeData: { [key in RevoltBadges]: UserBadge } = {
+  [RevoltBadges.Developer]: {
+    name: "Developer",
+    description: "Active or significant contributor to Revolt",
+    image: "https://app.revolt.chat/assets/badges/developer.svg",
+  },
+  [RevoltBadges.Translator]: {
+    name: "Translator",
+    description: "Helped translate Revolt",
+    image: "https://app.revolt.chat/assets/badges/translator.svg",
+  },
+  [RevoltBadges.Supporter]: {
+    name: "Supporter",
+    description: "Donated to Revolt",
+    image: "https://app.revolt.chat/assets/badges/supporter.svg",
+  },
+  [RevoltBadges.ResponsibleDisclosure]: {
+    name: "Responsibly Disclosed Bug(s)",
+    description: "Helped discover a security issue and responsibly disclosed it.",
+    image:
+      "https://autumn.revolt.chat/attachments/HyHA8YJ2oE34fGJPZIK9ZMORp6gF1_i-ER3f2YiveW/shield-regular-36.png",
+  },
+  [RevoltBadges.Founder]: {
+    name: "Founder",
+    description: "Founded Revolt.",
+    image: "https://app.revolt.chat/assets/badges/founder.svg",
+  },
+  [RevoltBadges.PlatformModeration]: {
+    name: "Platform Moderation",
+    description: "Part of the platform moderation team.",
+    image: "https://app.revolt.chat/assets/badges/moderation.svg",
+  },
+  [RevoltBadges.ActiveSupporter]: {
+    name: "Supporter",
+    description: "Donated to Revolt",
+    image: "https://app.revolt.chat/assets/badges/supporter.svg",
+  },
+  [RevoltBadges.Paw]: {
+    name: "🦊",
+    description: "paw",
+    image: "https://app.revolt.chat/assets/badges/paw.svg",
+  },
+  [RevoltBadges.EarlyAdopter]: {
+    name: "Early Adopter",
+    description: "Joined as one of the first 1k users.",
+    image: "https://app.revolt.chat/assets/badges/early_adopter.svg",
+  },
+  [RevoltBadges.ReservedRelevantJokeBadge1]: {
+    name: "sus",
+    description: "Whatever the funny joke is at any given time.",
+    image: "https://app.revolt.chat/assets/badges/amog.svg",
+  },
+  [RevoltBadges.ReservedRelevantJokeBadge2]: {
+    name: "It's Morbin Time",
+    description: "Whatever the funny joke is at any given time.",
+    image: "https://app.revolt.chat/assets/badges/amorbus.svg",
+  },
+};
