cabfc1a01ff3ec86802e9b6f4bb249e0b992ca4d
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-07 11:46:07 -0500

    start on user badges

---
+++ src/Badges.ts
@@ -0,0 +1,29 @@
+import { CustomBadge } from "./utils";
+
+/*
+ * Keep your image size relatively small. (<512px)
+ * CORS is not required as the image should be proxied clientside.
+ *
+ * It would be cool of you to comment your name by your ID to make it easier for others.
+ */
+
+export const CustomBadges: CustomBadge[] = [
+  {
+    //     StationaryStation             Bloom                         StationaryStation's Alt
+    ids: ["01G1V3VWVQFC8XAKYEPNYHHR2C", "01G205T4HKYNTK2H8QKR7BW02A", "01G5Z60R22C6TJJY7EJKG9ACD0"],
+    name: "Solenoid",
+    image: "https://autumn.revolt.chat/icons/7JNqQdO-7WfHyCrw69ct2nNwoM2QSH-RN4qOWWlrtP?max-side=256",
+  },
+  {
+    //     isnerd                        FatalErrorCoded
+    ids: ["01EX2NCWQ0CHS3QJF0FEQS1GR4", "01EXAME8DPWQH72RCBPX490ZMQ"],
+    name: "Revolt Founder",
+    image: "https://autumn.revolt.chat/icons/gtc0gJE2S3RvuDhrl2-JeakvgbqEGr2acvBnRTTh6k?max_side=256",
+  },
+  {
+    //     infi
+    ids: ["01F1WKM5TK2V6KCZWR6DGBJDTZ"],
+    name: "this is truly the",
+    image: "https://gifbox.me/favicon.png",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 export * as API from "revolt-api";
 export * from "./api";
+export * from "./Badges";
 export * from "./Client";
 export * from "./managers";
 export * from "./objects";
 export * from "./utils";
+export * from "./websocket";
+export * from "./websocketNotifications";
