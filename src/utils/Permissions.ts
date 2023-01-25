391018b5f44568318dc8a49b074a0eb8d12ced2d
Meow <50887230+itzTheMeow@users.noreply.github.com>
2023-01-25 14:30:48 -0500

    comments

--- src/utils/Permissions.ts
+++ src/utils/Permissions.ts
@@ -17,73 +17,73 @@ export enum UserPermissions {
 /** Permissions against servers/channels. */
 export enum Permissions {
   // * Generic permissions
-  /// Manage the channel or channels on the server
+  /** Manage the channel or channels on the server */
   ManageChannel = 2 ** 0,
-  /// Manage the server
+  /** Manage the server */
   ManageServer = 2 ** 1,
-  /// Manage permissions on servers or channels
+  /** Manage permissions on servers or channels */
   ManagePermissions = 2 ** 2,
-  /// Manage roles on server
+  /** Manage roles on server */
   ManageRole = 2 ** 3,
-  /// Manage server customisation (includes emoji)
+  /** Manage server customisation (includes emoji) */
   ManageCustomisation = 2 ** 4,
 
   // % 1 bits reserved
 
   // * Member permissions
-  /// Kick other members below their ranking
+  /** Kick other members below their ranking */
   KickMembers = 2 ** 6,
-  /// Ban other members below their ranking
+  /** Ban other members below their ranking */
   BanMembers = 2 ** 7,
-  /// Timeout other members below their ranking
+  /** Timeout other members below their ranking */
   TimeoutMembers = 2 ** 8,
-  /// Assign roles to members below their ranking
+  /** Assign roles to members below their ranking */
   AssignRoles = 2 ** 9,
-  /// Change own nickname
+  /** Change own nickname */
   ChangeNickname = 2 ** 10,
-  /// Change or remove other's nicknames below their ranking
+  /** Change or remove other's nicknames below their ranking */
   ManageNicknames = 2 ** 11,
-  /// Change own avatar
+  /** Change own avatar */
   ChangeAvatar = 2 ** 12,
-  /// Remove other's avatars below their ranking
+  /** Remove other's avatars below their ranking */
   RemoveAvatars = 2 ** 13,
 
   // % 7 bits reserved
 
   // * Channel permissions
-  /// View a channel
+  /** View a channel */
   ViewChannel = 2 ** 20,
-  /// Read a channel's past message history
+  /** Read a channel's past message history */
   ReadMessageHistory = 2 ** 21,
-  /// Send a message in a channel
+  /** Send a message in a channel */
   SendMessage = 2 ** 22,
-  /// Delete messages in a channel
+  /** Delete messages in a channel */
   ManageMessages = 2 ** 23,
-  /// Manage webhook entries on a channel
+  /** Manage webhook entries on a channel */
   ManageWebhooks = 2 ** 24,
-  /// Create invites to this channel
+  /** Create invites to this channel */
   InviteOthers = 2 ** 25,
-  /// Send embedded content in this channel
+  /** Send embedded content in this channel */
   SendEmbeds = 2 ** 26,
-  /// Send attachments and media in this channel
+  /** Send attachments and media in this channel */
   UploadFiles = 2 ** 27,
-  /// Masquerade messages using custom nickname and avatar
+  /** Masquerade messages using custom nickname and avatar */
   Masquerade = 2 ** 28,
-  /// React to messages with emoji
+  /** React to messages with emoji */
   React = 2 ** 29,
 
   // * Voice permissions
-  /// Connect to a voice channel
+  /** Connect to a voice channel */
   Connect = 2 ** 30,
-  /// Speak in a voice call
+  /** Speak in a voice call */
   Speak = 2 ** 31,
-  /// Share video in a voice call
+  /** Share video in a voice call */
   Video = 2 ** 32,
-  /// Mute other members with lower ranking in a voice call
+  /** Mute other members with lower ranking in a voice call */
   MuteMembers = 2 ** 33,
-  /// Deafen other members with lower ranking in a voice call
+  /** Deafen other members with lower ranking in a voice call */
   DeafenMembers = 2 ** 34,
-  /// Move members between voice channels
+  /** Move members between voice channels */
   MoveMembers = 2 ** 35,
 
   // * Misc. permissions
@@ -91,7 +91,7 @@ export enum Permissions {
   // % Bits 53 to 64: do not use
 
   // * Grant all permissions
-  /// Safely grant all permissions
+  /** Safely grant all permissions */
   GrantAllSafe = 0x000f_ffff_ffff_ffff,
 }
 
