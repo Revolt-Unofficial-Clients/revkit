cfb1fce12e29d59a098a1e1ff5a6d98f33fed6a8
Meow <me@itsmeow.cat>
2023-04-07 16:05:36 -0400

    v1.1.2 - Map Emitter docs and fixes

--- core/src/utils/MiniEmitter.ts
+++ core/src/utils/MiniEmitter.ts
@@ -7,16 +7,30 @@ export class MiniEmitter<T = any> {
 
   constructor() {}
 
+  /**
+   * Listen for an update to this Map.
+   * @param callback The callback to run when an update occurs.
+   * @returns `this`, useful for chaining.
+   */
   public onUpdate(callback: MiniEmitterCallback<T>) {
     this._callbacks.push(callback);
     return this;
   }
+  /**
+   * Stops listening for Map updates.
+   * @param callback The callback to stop listening for updates on.
+   * @returns `this`, useful for chaining.
+   */
   public offUpdate(callback: MiniEmitterCallback<T>) {
     const i = this._callbacks.indexOf(callback);
     if (i >= 0) this._callbacks.splice(i, 1);
     return this;
   }
-  protected fireUpdate(updated?: T[]) {
+  /**
+   * Runs all of the callbacks for this emitter. (meant to run when an update occurs)
+   * @param updated The objects that caused this update.
+   */
+  public fireUpdate(updated?: T[]) {
     this._callbacks.forEach((c) => c(updated || []));
   }
 }
@@ -27,16 +41,30 @@ export class MiniMapEmitter<T> extends Map<string, T> {
     super();
   }
 
+  /**
+   * Listen for an update to this Map.
+   * @param callback The callback to run when an update occurs.
+   * @returns `this`, useful for chaining.
+   */
   public onUpdate(callback: MiniEmitterCallback<T>) {
     this._callbacks.push(callback);
     return this;
   }
+  /**
+   * Stops listening for Map updates.
+   * @param callback The callback to stop listening for updates on.
+   * @returns `this`, useful for chaining.
+   */
   public offUpdate(callback: MiniEmitterCallback<T>) {
     const i = this._callbacks.indexOf(callback);
     if (i >= 0) this._callbacks.splice(i, 1);
     return this;
   }
-  protected fireUpdate(updated?: T[]) {
+  /**
+   * Runs all of the callbacks for this emitter. (meant to run when an update occurs)
+   * @param updated The objects that caused this update.
+   */
+  public fireUpdate(updated?: T[]) {
     this._callbacks.forEach((c) => c(updated || []));
   }
 }
