42261525902cd28207b246e53e32ca682d51a5f5
Meow <me@itsmeow.cat>
2023-04-08 19:24:04 -0400

    remove 'full'

--- voice/src/full.ts
+++
@@ -1,8 +0,0 @@
-/* This file is just so the docs export correctly. */
-
-export * from "./index";
-export * from "./msc";
-export { VoiceClientBrowser, VoiceClientNode };
-
-import VoiceClientBrowser from "./browser";
-import VoiceClientNode from "./node";
--- voice/src/index.ts
+++ voice/src/index.ts
@@ -1,3 +1,7 @@
 export * from "./Signaling";
 export * from "./VoiceClient";
+export * from "./msc";
 export * from "./types";
+export { VoiceClientBrowser, VoiceClientNode };
+import VoiceClientBrowser from "./browser";
+import VoiceClientNode from "./node";
