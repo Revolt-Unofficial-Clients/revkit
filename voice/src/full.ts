62793aca0e87f8cc8c67cf3a4726ae5175516672
Meow <me@itsmeow.cat>
2023-04-08 01:21:23 -0400

    docs entrypoints

---
+++ voice/src/full.ts
@@ -0,0 +1,6 @@
+export * from "./index";
+export * from "./msc";
+
+import VoiceClientBrowser from "./browser";
+import VoiceClientNode from "./node";
+export { VoiceClientBrowser, VoiceClientNode };
